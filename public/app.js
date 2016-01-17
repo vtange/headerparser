bb34ae983dd4b351092ab08ea521e681f10faca6
Victor Tang <victortang1992@gmail.com>
2016-01-17 15:43:29 -0800

    show header data on browser

--- public/app.js
+++ public/app.js
@@ -3,8 +3,9 @@
   var app = angular.module('headerparser', []);
 
 app.controller('MainCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
+	$scope.headerdata;
 	$scope.getHeader = function(){
-		$http.post($window.location.href+"api/whoami/").success(function(data){console.log(data);}).error(function(err){throw err;});
+		$http.post($window.location.href+"api/whoami/").success(function(data){console.log(data);$scope.headerdata=data;}).error(function(err){throw err;});
 	};
 }]);//end of controller
   //end of function
