1c9dde478adb68e96c5d1721bfda5c143e0673c8
Victor Tang <victortang1992@gmail.com>
2016-01-17 15:52:17 -0800

    regex filter out extraneous info

--- routes/routes.js
+++ routes/routes.js
@@ -9,26 +9,30 @@ module.exports = function(app) {
         res.render('index.ejs'); // load the index.ejs file
     });
 	app.get('/api/whoami', function(req, res) {
+		var langCapt = /([a-z]{2}-[A-Z]{2})/g;
+		var softCapt = /\((.*)\)/g;
 		var returnvalue = {
 			"ipaddress": req.ip ||
 					 req.connection.remoteAddress || 
 					 req.socket.remoteAddress ||
 					 req.connection.socket.remoteAddress ||
 					req.headers['x-forwarded-for'],
-			"language": req.headers['accept-language'],
-			"software": req.headers['user-agent'],
+			"language": req.headers['accept-language'].match(langCapt)[0],
+			"software": softCapt.exec(req.headers['user-agent'])[1],
 		}
         res.send(returnvalue); // load the index.ejs file
     });
 	app.post('/api/whoami', function(req, res) {
+		var langCapt = /([a-z]{2}-[A-Z]{2})/g;
+		var softCapt = /\((.*)\)/g;
 		var returnvalue = {
 			"ipaddress": req.ip ||
 					 req.connection.remoteAddress || 
 					 req.socket.remoteAddress ||
 					 req.connection.socket.remoteAddress ||
 					req.headers['x-forwarded-for'],
-			"language": req.headers['accept-language'],
-			"software": req.headers['user-agent'],
+			"language": req.headers['accept-language'].match(langCapt)[0],
+			"software": softCapt.exec(req.headers['user-agent'])[1],
 		}
         res.send(JSON.stringify(returnvalue)); // send STRING with JSON data inside
     });
